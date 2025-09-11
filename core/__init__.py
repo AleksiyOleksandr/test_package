5f5e1c6a2d6b149ec8b03b013ac87202f4a54a8a
AleksiyOleksandr <sasha.aleksyj@gmail.com>
2025-09-11 11:38:39 +0300

    fix imports in core/init

--- core/__init__.py
+++ core/__init__.py
@@ -6,18 +6,18 @@ from .Fields.datetime import DateTime
 from .base import Base
 from .config_manager import ConfigManager
 from .counter import Counter
-from data_field import DataField
-from document import Document
-from entity import Entity
-from event import Event
-from inforecord import InfoRecord
-from object import Object
-from Record import Record
-from reference import Reference
-from sync_nodes import SyncNode
-from table_section import TableSection
-from table_section_record import TableSectionRecord
-from tables import Tables
+from .data_field import DataField
+from .document import Document
+from .entity import Entity
+from .event import Event
+from .inforecord import InfoRecord
+from .object import Object
+from .Record import Record
+from .reference import Reference
+from .sync_nodes import SyncNode
+from .table_section import TableSection
+from .table_section_record import TableSectionRecord
+from .tables import Tables
 
 __all__ = ["Reference", "String", "Boolean", "Number", "DateTime",
            "Base", "ConfigManager", "Counter", "DataField", "Document",
