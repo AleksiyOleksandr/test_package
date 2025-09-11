b0d337557bc7da70abfba4a1290e3c0e2bff6c50
AleksiyOleksandr <sasha.aleksyj@gmail.com>
2025-09-11 12:12:55 +0300

    Додав логіку створення файлу в файл config_namager

--- core/config_manager.py
+++ core/config_manager.py
@@ -1,6 +1,21 @@
 import json
 import os
 
+data = {
+  "db": {
+    "type": "",
+    "name": "",
+    "user": "",
+    "pwd": "",
+    "host": ""
+  }
+}
+
+# Абсолютний шлях до кореня
+root_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Шлях до файлу в корені
+file_path = os.path.join(root_dir, "data.json")
 
 class ConfigManager():
     _instance = None
@@ -11,11 +26,17 @@ class ConfigManager():
             cls._instance = super().__new__(cls)
         return cls._instance
 
-    def load(self, path = 'config.json'):
+    def __createFile(self):
+        with open(file_path, "w", encoding="utf-8") as file:
+            json.dump(data, file)
+
+    def load(self):
         if self._config is None:
-            if not os.path.isfile(path):
-                raise FileNotFoundError(f"Файл {path} не знайдено ⚠")
-            with open(path, 'r', encoding='utf-8') as file:
+            if not os.path.isfile(file_path):
+                print('Конфігураційний файл не створено!')
+                print('Створюю файл config.json в корені проєкту)')
+                print('Файл створено:) Заповніть будь-ласка файл!')
+            with open(file_path, 'r', encoding='utf-8') as file:
                 self._config = json.load(file)
         return self._config
 
